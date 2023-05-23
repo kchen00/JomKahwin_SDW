2b9ba42947ff27367b31ba5a3c0ab1ca27fc83d2
kalvin chen <kalvin412@gmail.com>
2023-05-23 14:00:05 +0800

    added a homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <h1>Selamat datang ke Sistem Jom Kahwin</h1>
+    <div class="nav_bar">
+        <a href="/">Laman Utama</a>
+        <a href="/contact">Hubungi Kami</a>
+    </div>
+</html>
