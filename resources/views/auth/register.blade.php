98769cfe1558ab847a0af53e3c864248eeddfa46
kalvin chen <kalvin412@gmail.com>
2023-05-23 16:19:09 +0800

    modified the account register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,23 +2,23 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <!-- Ic number -->
+        <div class="mt-4">
+            <x-input-label for="ic_number" :value="__('Nombor IC')" />
+            <x-text-input id="ic_number" class="block mt-1 w-full" type="text" name="ic_number" :value="old('ic_number')" required autofocus autocomplete="ic_number" />
+            <x-input-error :messages="$errors->get('ic_number')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="ic_number" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Kata Laluan')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,13 +30,21 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Ulang Kata Laluan')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+    
+        <!-- Gender -->
+        <div class="mt-4">
+            <x-input-label for="gender" :value="__('Jantina')" />
+            {{-- <x-dropdown/>  --}}
+            <x-text-input id="gender" class="block mt-1 w-full" type="gender" name="gender" :value="old('gender')" required autocomplete="gender" />
+            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
@@ -45,7 +53,7 @@
             </a>
 
             <x-primary-button class="ml-4">
-                {{ __('Register') }}
+                {{ __('Daftar akaun') }}
             </x-primary-button>
         </div>
     </form>
