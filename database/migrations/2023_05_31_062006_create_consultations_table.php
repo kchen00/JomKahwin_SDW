0ca40b1099bfd7377819999b970a30bdd54ca5de
Humaira <hmr.sykirin@gmail.com>
2023-06-01 01:34:02 +0800

    request consultation

---
+++ database/migrations/2023_05_31_062006_create_consultations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultations', function (Blueprint $table) {
+            $table->id("CO_consultationID");
+            $table->integer("SA_advisorID");
+            $table->foreignId("SA_advisorID")->references("SA_advisorID")->on("SA_serviceAdvisor");
+            $table->integer("U_userID");
+            $table->foreignId("U_userID")->references("U_userID")->on("users");
+            $table->dateTime("CO_consultationDateTimeStart");
+            $table->dateTime("CO_consultationDateTimeEnd");
+            $table->string("CO_consultationLocation", 50);
+            $table->string("CO_consultationRemarks", 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultations');
+    }
+};
