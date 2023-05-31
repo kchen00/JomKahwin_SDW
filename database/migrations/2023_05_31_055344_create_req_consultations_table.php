8987bbe5f4e726ba11db00f6b445f674854eb03b
Humaira <hmr.sykirin@gmail.com>
2023-05-31 14:16:34 +0800

    made request consultation controller and migration

---
+++ database/migrations/2023_05_31_055344_create_req_consultations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('req_consultations', function (Blueprint $table) {
+            $table->id("RC_reqConsultation");
+            $table->integer("U_userID");
+            $table->foreign("U_userID")->references("U_userID")->on("users");
+            $table->string("RC_purpose", 50);
+            $table->string("RC_paidChoice", 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('req_consultations');
+    }
+};
