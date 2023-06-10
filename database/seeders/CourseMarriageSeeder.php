526f525fd290dda64306529995de86203cb2aae6
warsena <dnaawarsena@gmail.com>
2023-06-10 15:15:54 +0800

    create seeder

--- database/seeders/CourseMarriageSeeder.php
+++ database/seeders/CourseMarriageSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\CourseMarriage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class MMarriageSeeder extends Seeder
+class CourseMarriageSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        MMariage::create([
+        CourseMarriage::create([
 
         ]);
     }
